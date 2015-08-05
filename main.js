dc949c3585038384c72eb4bee47ff09c8e6e7aae
Michael Ion <e1005233@student.tuwien.ac.at>
2015-08-06 00:28:15 +0200

    added color animation

--- main.js
+++ main.js
@@ -5,9 +5,53 @@
 var dict_name = ["Kolonig", "asdf", "ww"];
 var used_name = [];
 var dict_meaning = ["This is a super long description of alsdfkij aslfkijasoflijasdof ijasdof ijasdof ijsdflkrejfl kejfl kej lkej sl j","2","3"];
+var colorSelect = -1;
 
+
+function css(selector, property, value) {
+    for (var i=0; i<document.styleSheets.length;i++) {//Loop through all styles
+        //Try add rule
+		console.log(selector+ ' {'+property+':'+value+'}');
+        try { document.styleSheets[i].insertRule(selector+ ' {'+property+':'+value+'}', document.styleSheets[i].cssRules.length);
+        } catch(err) {try { document.styleSheets[i].addRule(selector, property+':'+value);} catch(err) {}}//IE
+    }
+}
+
+
+function setRandomStyle() {
+	/*There are two colors to set:
+	First: Word-Bg
+	Second: Body-Background, Description-Bg, NewButton-Bg
+	Font color is white*/
+	
+	//don't change font color
+	var values = [['#7D9ABC', '#2C3E50'],
+				['#427F3F', '#4CB646']
+				//['#', '#'],
+				/*['#', '#'],
+				['#', '#'],
+				['#', '#'],
+				['#', '#'],
+				['#', '#'],
+				['#', '#'],
+				['#', '#'],
+				['#', '#']*/
+	]
+	var selectors = ['body', '#newWordButton', '.name', '.desc'];
+
+	//make fix for first, then animate for every following
+	var tmp = Math.floor(Math.random()*values.length);
+	colorSelect = tmp == colorSelect ? (tmp+1)%values.length : tmp;
+	$(selectors[0]).animate({'background-color': values[colorSelect][1]}, { duration: 200, queue: false });
+	$(selectors[1]).animate({'background-color': values[colorSelect][1]}, { duration: 200, queue: false });
+	$(selectors[2]).animate({'background-color': values[colorSelect][0]}, { duration: 200, queue: false });
+	$(selectors[3]).animate({'background-color': values[colorSelect][1]}, { duration: 200, queue: false });
+	css(selectors[3], 'background-color', values[colorSelect][1]);
+
+}
 
 function main() {
+	setRandomStyle();
 	fitText(document.getElementById('fittext'), 1.2);
 	loadNewWord();
 	var names = document.getElementsByClassName("name");
